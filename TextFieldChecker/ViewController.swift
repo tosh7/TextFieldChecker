3c30136c12dcd1acac787c864d769be34abd9836
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-22 01:53:19 +0900

    Initial Commit

---
+++ TextFieldChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldChecker
+//
+//  Created by Satoshi Komatsu on 2019/10/22.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
