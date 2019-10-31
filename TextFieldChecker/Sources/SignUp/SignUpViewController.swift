34fbc4bd92d7e3e7733bb810a31bd86c666e113d
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-31 21:32:14 +0900

    made  SignUpScene

--- TextFieldChecker/Sources/SignUp/SignUpViewController.swift
+++ TextFieldChecker/Sources/SignUp/SignUpViewController.swift
@@ -35,6 +35,8 @@ final class SignUpViewController: UIViewController {
     
     private let viewModel: SignUpViewModelType
     private let navigator: SignUpNavigatorType
+    private var emailValidation: Validation = .empty
+    private var passwordValidation: Validation = .empty
     
     private var emailTextField: CustomTextFieldView = {
         let view = CustomTextFieldView()
@@ -43,15 +45,49 @@ final class SignUpViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    private var passwordTextField: CustomTextFieldView = {
+        let view = CustomTextFieldView()
+        view.textField.placeholder = "このTextFieldはPasswprd用"
+        view.label.text = "ここにきちんと文字が出るかを確認"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self,
+                         action: #selector(registerButtonTapped),
+                         for: .touchUpInside)
+        button.titleLabel?.text = "登録"
+        button.isEnabled = false
+        button.backgroundColor = UIColor.red
+        return button
+    }()
+    
+    @objc func registerButtonTapped() {
+        print("REGISTER")
+    }
 }
 
 extension SignUpViewController {
     private func setUpView() {
         view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(registerButton)
         
-        emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        emailTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
-//        emailTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20).isActive = true
+        
+        passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20).isActive = true
+        
+        registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        registerButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
