d00b05796100acb74c6c48321ffa564b8bbbedc8
tosh7 <zlia.6.lj.425@gmail.com>
2019-11-01 01:58:54 +0900

    とりまCommit

--- TextFieldChecker/Sources/SignUp/SignUpViewModel.swift
+++ TextFieldChecker/Sources/SignUp/SignUpViewModel.swift
@@ -11,11 +11,15 @@ import RxSwift
 import RxCocoa
 
 protocol SignUpViewModelInputs {
-    
+    func emailTextFieldDidChange(_ email: String)
+    func passwordTextFieldDidChange(_ password: String)
+    func register()
 }
 
 protocol SignUpViewModelOutputs {
-    
+    var emailValidation: Driver<Validation> { get }
+    var passwordvalidation: Driver<Validation> { get }
+    var registerButtonIsEnabled: Driver<Bool> { get }
 }
 
 protocol SignUpViewModelType {
@@ -24,8 +28,37 @@ protocol SignUpViewModelType {
 }
 
 final class SignUpViewModel: SignUpViewModelType, SignUpViewModelInputs, SignUpViewModelOutputs {
-    init() {}
+    init() {
+        emailValidation = emailProperty
+            .filter { !$0.isEmpty }
+            .map { email -> Validation in
+                return .success
+        }.asDriver(onErrorJustReturn: .error(with: "謎のエラー"))
+        passwordvalidation = passwordProperty
+            .filter { !$0.isEmpty }
+            .map { password -> Validation in
+                return .success
+        }.asDriver(onErrorJustReturn: .error(with: "謎のエラー"))
+    }
     
+    private let emailProperty = BehaviorRelay<String>(value: "")
+    func emailTextFieldDidChange(_ email: String) {
+        emailProperty.accept(email)
+    }
+    
+    private let passwordProperty = BehaviorRelay<String>(value: "")
+    func passwordTextFieldDidChange(_ password: String) {
+        passwordProperty.accept(password)
+    }
+    
+    private let registerProperty = PublishSubject<Void>()
+    func register() {
+        registerProperty.onNext(())
+    }
+    
+    let emailValidation: Driver<Validation>
+    let passwordvalidation: Driver<Validation>
+    let registerButtonIsEnabled: Driver<Bool>
     var inputs: SignUpViewModelInputs { return self }
     var outputs: SignUpViewModelOutputs { return self }
 }
